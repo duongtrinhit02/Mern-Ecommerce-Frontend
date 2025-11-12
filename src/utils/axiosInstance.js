1af08f54c98727facd2dc3956f7392fba452b6d0
duongtrinhit02 <duongtrinhit02@gmail.com>
2025-11-12 10:54:38 +0700

    update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api`
-  : 'http://localhost:5000/api';
+// Nếu đang chạy local (localhost), dùng backend local
+// Nếu đang deploy, dùng backend deploy
+const baseURL = window.location.hostname === 'localhost'
+  ? 'http://localhost:5000/api'
+  : 'https://mern-ecommerce-backend-ptjk.onrender.com/api';
 
-console.log("Axios baseURL:", baseURL); 
+console.log("Axios baseURL:", baseURL);
 
 const instance = axios.create({
   baseURL,
-  withCredentials: true, 
+  withCredentials: true, // để gửi cookies nếu cần
 });
 
-
+// Gắn token tự động
 instance.interceptors.request.use(
   (config) => {
     const storedAuth = localStorage.getItem('auth');
